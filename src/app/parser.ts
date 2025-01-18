df3f496b5754d485a62927bbd7dba5df04b5fd81
Alvis Ng <alvis.ng.business@gmail.com>
2025-01-18 13:46:25 +0800

    Add Parser

---
+++ src/app/parser.ts
@@ -0,0 +1,31 @@
+import { Viewability, Video } from '@/app/types'
+
+type vidObj = {
+    [key: string]: string;
+}
+
+const parsePost = (text: string) : Video => {
+    const vidObj : vidObj = text.split('\n').reduce((acc, str) => {
+        const [key, value] = str.split(':').map(item => item.trim());
+        acc[key] = value;
+        return acc;
+    }, {} as { [key: string]: string });
+
+    return {
+        date: vidObj['Date'],
+        url: vidObj['Link'],
+        likes: vidObj['Like(s)'],
+        whoCanView: vidObj['Who can view'] == 'Public'
+        ? Viewability.Public : vidObj['Who can view'] == 'Friends'
+        ? Viewability.Friends : Viewability.Private,
+        canComment: vidObj['Allow comments'] == 'Yes',
+        canStitch: vidObj['Allow stitches'] == 'Yes',
+        canDuet: vidObj['Allow duets'] == 'Yes',
+        canSticker: vidObj['Allow stickers'] == 'Yes',
+        canShareToStory: vidObj['Allow sharing to story'] == 'Yes',
+    }
+}
+
+export const parsePostTXT = (text: string) : Video[] => {
+    return text.split("\n\n").map(x => parsePost(x))
+}
